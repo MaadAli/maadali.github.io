e093e2c20a1c5d749b19d0a8dfd939a2ccf23143
maadmughal <maadmughal@gmail.com>
2022-09-15 06:59:52 +0500

    build push

---
+++ precache-manifest.fbf9bd1c5516ab6fb7a14e3b7563b5a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbde2d9b9327e0a369450f8926dc8f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4002b240da3a367088",
+    "url": "/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "0b91b21081964e6c1e2d",
+    "url": "/static/css/main.9bcab7a8.chunk.css"
+  },
+  {
+    "revision": "fb4002b240da3a367088",
+    "url": "/static/js/2.89441ecd.chunk.js"
+  },
+  {
+    "revision": "c40be130369555ab6cc7391c28632b27",
+    "url": "/static/js/2.89441ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b91b21081964e6c1e2d",
+    "url": "/static/js/main.2a35840a.chunk.js"
+  },
+  {
+    "revision": "0285b6b4464d1600919e",
+    "url": "/static/js/runtime-main.7adca32c.js"
+  }
+]);
